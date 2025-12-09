80b4b88cb466058802fc71add73fba404ebfe1f4
Hiroki Yamada <yamada4tec@gmail.com>
2025-12-09 11:29:56 +0900

    Update VR avatar

--- Assets/Competition/InteractiveCleanup/Scripts/CleanupAvatarVRController.cs
+++ Assets/Competition/InteractiveCleanup/Scripts/CleanupAvatarVRController.cs
@@ -1,19 +1,18 @@
 using SIGVerse.Common;
 using SIGVerse.Human.IK;
 using SIGVerse.Human.VR;
-using SIGVerse.SIGVerseRosBridge;
-using SIGVerse.ToyotaHSR;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
-
+using UnityEngine.InputSystem.XR;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.Management;
-using Valve.VR;
-using Valve.VR.InteractionSystem;
 
 namespace SIGVerse.Competition.InteractiveCleanup
 {
@@ -27,11 +26,14 @@ namespace SIGVerse.Competition.InteractiveCleanup
 
 	public class CleanupAvatarVRController : MonoBehaviour
 	{
+		//public GameObject avatar;
+		public GameObject xrOrigin;
+
 		public Laser laserLeft;
 		public Laser laserRight;
 
-		public GameObject cameraRig;
-		public GameObject eyeAnchor;
+		//public GameObject cameraRig;
+		//public GameObject eyeAnchor;
 
 		public Animator   avatarAnimator;
 
@@ -49,19 +51,20 @@ namespace SIGVerse.Competition.InteractiveCleanup
 
 		private SimpleHumanVRController simpleHumanVRController;
 		private SimpleIK simpleIK;
-		private List<CleanupAvatarVRHandController> cleanupAvatarVRHandControllers;
+		private List<HumanHandController> vrHandControllers;
 
 		private XRLoader activeLoader;
-
+		private InputDevice leftHandDevice;
+		private InputDevice rightHandDevice;
 
 		void Awake()
 		{
 			this.capsuleColliders      = this.GetComponentsInChildren<CapsuleCollider>().ToList();
 			this.capsuleColliders.Remove(this.rootCapsuleCollider);
 
-			this.simpleHumanVRController        = this.GetComponentInChildren<SimpleHumanVRController>();
-			this.simpleIK                       = this.GetComponentInChildren<SimpleIK>();
-			this.cleanupAvatarVRHandControllers = this.GetComponentsInChildren<CleanupAvatarVRHandController>().ToList();
+			this.simpleHumanVRController = this.GetComponentInChildren<SimpleHumanVRController>();
+			this.simpleIK                = this.GetComponentInChildren<SimpleIK>();
+			this.vrHandControllers       = this.GetComponentsInChildren<HumanHandController>().ToList();
 
 			ExecutionMode executionMode = (ExecutionMode)Enum.ToObject(typeof(ExecutionMode), CleanupConfig.Instance.configFileInfo.executionMode);
 
@@ -70,7 +73,8 @@ namespace SIGVerse.Competition.InteractiveCleanup
 				// For the competition. Read generated data.
 				case ExecutionMode.Competition:
 				{
-					this.cameraRig.SetActive(false);
+					//this.cameraRig.SetActive(false);
+					this.xrOrigin.SetActive(false);
 					this.avatarAnimator.enabled = false;
 
 					this.rootCapsuleCollider.enabled = false;
@@ -78,14 +82,12 @@ namespace SIGVerse.Competition.InteractiveCleanup
 
 					this.simpleHumanVRController.enabled = false;
 					this.simpleIK               .enabled = false;
-					foreach(CleanupAvatarVRHandController cleanupAvatarVRHandController in this.cleanupAvatarVRHandControllers){ cleanupAvatarVRHandController.enabled = false; }
+					foreach(HumanHandController vrHandController in this.vrHandControllers){ vrHandController.enabled = false; }
 
 					this.initialPositionMarker.SetActive(false);
 
-					// Add for SteamVR START
 					Rigidbody[] rigidbodies = this.GetComponentsInChildren<Rigidbody>(true);
 					foreach(Rigidbody rigidbody in rigidbodies){ rigidbody.useGravity = false; }
-					// Add for SteamVR END
 
 					this.enabled = false;
 
@@ -119,7 +121,7 @@ namespace SIGVerse.Competition.InteractiveCleanup
 
 			if (xrManagerSettings == null) { SIGVerseLogger.Error("xrManagerSettings == null"); yield break; }
 
-			if(xrManagerSettings.activeLoader == null)
+			if (xrManagerSettings.activeLoader == null)
 			{
 				yield return xrManagerSettings.InitializeLoader();
 			}
@@ -134,12 +136,12 @@ namespace SIGVerse.Competition.InteractiveCleanup
 
 			xrManagerSettings.activeLoader.Start();
 
-			SteamVR_Actions.sigverse.Activate(SteamVR_Input_Sources.Any);
+			//SteamVR_Actions.sigverse.Activate(SteamVR_Input_Sources.Any);
 		}
 
 		private void EnableScriptsForDataGeneration()
 		{
-			this.cameraRig.SetActive(true);
+			this.xrOrigin.SetActive(true);
 			this.avatarAnimator.enabled = true;
 
 			this.rootCapsuleCollider.enabled = true;
@@ -147,35 +149,42 @@ namespace SIGVerse.Competition.InteractiveCleanup
 
 			this.simpleHumanVRController.enabled = true;
 			this.simpleIK               .enabled = true;
-			foreach(CleanupAvatarVRHandController cleanupAvatarVRHandController in this.cleanupAvatarVRHandControllers){ cleanupAvatarVRHandController.enabled = true; }
+			foreach(HumanHandController vrHandController in this.vrHandControllers){ vrHandController.enabled = true; }
 
 			this.initialPositionMarker.SetActive(true);
 
+			// Enable VR Scripts
+			this.xrOrigin.GetComponentInChildren<XROrigin>().enabled = true;
+			this.xrOrigin.GetComponentInChildren<InputActionManager>().enabled = true;
 
-			// Add for SteamVR START
-			this.GetComponent<Player>().enabled = true;
+			this.xrOrigin.GetComponentInChildren<Camera>().enabled = true;
+//			this.xrOrigin.GetComponentInChildren<AudioListener>().enabled = true;
+			Array.ForEach(this.xrOrigin.GetComponentsInChildren<TrackedPoseDriver>(), x => x.enabled = true);
+			this.xrOrigin.GetComponentInChildren<SIGVerse.Human.IK.AnchorPostureCalculator>().enabled = true;
 
-			this.cameraRig.GetComponent<SIGVerse.Human.IK.AnchorPostureCalculator>().enabled = true;
 
-			SteamVR_Behaviour_Pose[] steamVrBehaviourPoses = this.cameraRig.GetComponentsInChildren<SteamVR_Behaviour_Pose>();
-			foreach(SteamVR_Behaviour_Pose steamVrBehaviourPose in steamVrBehaviourPoses){ steamVrBehaviourPose.enabled = true;}
+			StartCoroutine(SIGVerseUtils.GetXrDevice(XRNode.LeftHand,  x => this.leftHandDevice  = x));
+			StartCoroutine(SIGVerseUtils.GetXrDevice(XRNode.RightHand, x => this.rightHandDevice = x));
 
-			Hand[] hands = this.cameraRig.GetComponentsInChildren<Hand>(true);
-			foreach(Hand hand in hands){ hand.enabled = true; }
+			//this.GetComponent<Player>().enabled = true;
 
-			this.eyeAnchor.GetComponent<Camera>().enabled = true;
-			this.eyeAnchor.GetComponent<SteamVR_CameraHelper>().enabled = true;
-			// Add for SteamVR END
+			//this.cameraRig.GetComponent<SIGVerse.Human.IK.AnchorPostureCalculator>().enabled = true;
+
+			//SteamVR_Behaviour_Pose[] steamVrBehaviourPoses = this.cameraRig.GetComponentsInChildren<SteamVR_Behaviour_Pose>();
+			//foreach(SteamVR_Behaviour_Pose steamVrBehaviourPose in steamVrBehaviourPoses){ steamVrBehaviourPose.enabled = true;}
+
+			//Hand[] hands = this.cameraRig.GetComponentsInChildren<Hand>(true);
+			//foreach(Hand hand in hands){ hand.enabled = true; }
+
+			//this.eyeAnchor.GetComponent<Camera>().enabled = true;
+			//this.eyeAnchor.GetComponent<SteamVR_CameraHelper>().enabled = true;
 		}
 
-
-//#if SIGVERSE_USING_OCULUS_RIFT
-
-		// Update is called once per frame
 		void Update()
 		{
 			// Enable/Disable Laser of Left hand
-			if (SteamVR_Actions.sigverse_SqueezeMiddle.GetAxis(SteamVR_Input_Sources.LeftHand) > 0.95)
+//			if (SteamVR_Actions.sigverse_SqueezeMiddle.GetAxis(SteamVR_Input_Sources.LeftHand) > 0.95)
+			if(this.leftHandDevice.TryGetFeatureValue(CommonUsages.grip, out float leftHandTriggerValue) && leftHandTriggerValue > 0.95)
 			{
 				if(!this.laserLeft.gameObject.activeInHierarchy)
 				{
@@ -191,7 +200,8 @@ namespace SIGVerse.Competition.InteractiveCleanup
 			}
 
 			// Enable/Disable Laser of Right hand
-			if (SteamVR_Actions.sigverse_SqueezeMiddle.GetAxis(SteamVR_Input_Sources.RightHand) > 0.95)
+//				if (SteamVR_Actions.sigverse_SqueezeMiddle.GetAxis(SteamVR_Input_Sources.RightHand) > 0.95)
+			if (this.rightHandDevice.TryGetFeatureValue(CommonUsages.grip, out float rightHandTriggerValue) && rightHandTriggerValue > 0.95)
 			{
 				if(!this.laserRight.gameObject.activeInHierarchy)
 				{
@@ -207,42 +217,50 @@ namespace SIGVerse.Competition.InteractiveCleanup
 			}
 
 
-			if (this.laserLeft.gameObject.activeInHierarchy && SteamVR_Actions.sigverse_PressIndex.GetStateDown(SteamVR_Input_Sources.LeftHand))
+			if (this.laserLeft.gameObject.activeInHierarchy)
 			{
-				string selectedTargetName = this.laserLeft.Point(true);
-
-				foreach (GameObject avatarMotionDestination in this.avatarMotionDestinations)
+//				if (SteamVR_Actions.sigverse_PressIndex.GetStateDown(SteamVR_Input_Sources.LeftHand))
+				if(this.leftHandDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool leftTriggerButton) && leftTriggerButton) // Index finger
 				{
-					ExecuteEvents.Execute<IAvatarMotionHandler>
-					(
-						target: avatarMotionDestination,
-						eventData: null,
-						functor: (reciever, eventData) => reciever.OnAvatarPointByLeft()
-					);
-				}
+					string selectedTargetName = this.laserLeft.Point(true);
 
-				Debug.Log("selectedTargetName=" + selectedTargetName);
+					foreach (GameObject avatarMotionDestination in this.avatarMotionDestinations)
+					{
+						ExecuteEvents.Execute<IAvatarMotionHandler>
+						(
+							target: avatarMotionDestination,
+							eventData: null,
+							functor: (reciever, eventData) => reciever.OnAvatarPointByLeft()
+						);
+					}
+
+					Debug.Log("selectedTargetName=" + selectedTargetName);
+				}
 			}
 
-
-			if (this.laserRight.gameObject.activeInHierarchy && SteamVR_Actions.sigverse_PressIndex.GetStateDown(SteamVR_Input_Sources.RightHand))
+			if (this.laserRight.gameObject.activeInHierarchy)
 			{
-				string selectedTargetName = this.laserRight.Point(true);
-
-				foreach (GameObject avatarMotionDestination in this.avatarMotionDestinations)
+//				if (SteamVR_Actions.sigverse_PressIndex.GetStateDown(SteamVR_Input_Sources.RightHand))
+				if(this.rightHandDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool rightTriggerButton) && rightTriggerButton) // Index finger
 				{
-					ExecuteEvents.Execute<IAvatarMotionHandler>
-					(
-						target: avatarMotionDestination,
-						eventData: null,
-						functor: (reciever, eventData) => reciever.OnAvatarPointByRight()
-					);
-				}
+					string selectedTargetName = this.laserRight.Point(true);
 
-				Debug.Log("selectedTargetName=" + selectedTargetName);
+					foreach (GameObject avatarMotionDestination in this.avatarMotionDestinations)
+					{
+						ExecuteEvents.Execute<IAvatarMotionHandler>
+						(
+							target: avatarMotionDestination,
+							eventData: null,
+							functor: (reciever, eventData) => reciever.OnAvatarPointByRight()
+						);
+					}
+
+					Debug.Log("selectedTargetName=" + selectedTargetName);
+				}
 			}
 
-			if(SteamVR_Actions.sigverse_PressNearButton.GetStateDown(SteamVR_Input_Sources.RightHand))
+//			if(SteamVR_Actions.sigverse_PressNearButton.GetStateDown(SteamVR_Input_Sources.RightHand))
+			if(this.rightHandDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool rightPrimaryButton) && rightPrimaryButton)
 			{
 				foreach (GameObject avatarMotionDestination in this.avatarMotionDestinations)
 				{
@@ -257,7 +275,8 @@ namespace SIGVerse.Competition.InteractiveCleanup
 				Debug.Log("Pressed A button");
 			}
 
-			if(SteamVR_Actions.sigverse_PressNearButton.GetStateDown(SteamVR_Input_Sources.LeftHand))
+//			if(SteamVR_Actions.sigverse_PressNearButton.GetStateDown(SteamVR_Input_Sources.LeftHand))
+			if(this.leftHandDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool leftPrimaryButton) && leftPrimaryButton)
 			{
 				foreach (GameObject avatarMotionDestination in this.avatarMotionDestinations)
 				{
@@ -274,13 +293,15 @@ namespace SIGVerse.Competition.InteractiveCleanup
 		}
 		void OnDestroy()
 		{
-			// It is mandatory to perform this termination process.
-			if(this.activeLoader != null)
+			var xrManagerSettings = XRGeneralSettings.Instance.Manager;
+
+			if (xrManagerSettings == null) { return; }
+
+			if(xrManagerSettings.activeLoader != null)
 			{
-				this.activeLoader.Stop();
+				xrManagerSettings.activeLoader.Stop();
 				XRGeneralSettings.Instance.Manager.DeinitializeLoader();
 			}
 		}
-//#endif
 	}
 }
